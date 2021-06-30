6ad1584878856479f7eca4c7c68bf8529a0c8aaf
CyrusWarner <cyruswarner27@gmail.com>
2021-06-30 12:02:57 -0500

    Added variables to Customer model.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,3 +7,12 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    weekly_pickup_day = models.CharField(max_length=50)
+    onetime_pickup_date = models.DateField(null=True, blank=True)
+    start_suspension_date = models.DateField(null=True, blank=True)
+    end_suspension_date = models.DateField(null=True, blank=True)
+    customer_balance = models.IntegerField(default=0)
+    customer_address = models.CharField(max_length=50)
+    customer_zip_code = models.CharField(max_length=50)
+    weekly_pickup_confirmed = True
+    suspension = False
