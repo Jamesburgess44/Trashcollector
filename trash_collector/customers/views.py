f3150a3efd1d41b4dd9ac228eb66ec1bbe737a7a
CyrusWarner <cyruswarner27@gmail.com>
2021-07-01 15:51:52 -0500

    Added user id to customer model.

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
 from .models import Customer
-from .forms import customer_forms
+from .forms import customer_forms, change_pickup_form
+from django.urls import reverse
+
+
 # Create your views here.
 
 # TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
@@ -16,26 +19,70 @@ def index(request):
     # thereby finding the customer/employee profile that matches with the logged-in user.
     return render(request, 'customers/index.html')
 
+
 def customer_signup(request):
-    user_id = request.user.id
-    form = customer_forms(request.POST)
+    if request.method == 'POST':
+        customer_name = request.POST.get('name')
+        customer_address = request.POST.get('customer_address')
+        customer_zip_code = request.POST.get('customer_zip_code')
+        customer_weekly_pickup_day = request.POST.get('weekly_pickup_day')
+        new_customer = Customer(name=customer_name, customer_address=customer_address, customer_zip_code=customer_zip_code, weekly_pickup_day=customer_weekly_pickup_day, user=request.user)
+        new_customer.save()
+
+        return HttpResponseRedirect(reverse('customers:customer_signup'))
+    else:
+        return render(request, 'customers/customer_signup_information.html')
+
+
+
+
+def customer_account_info(request, user_id):
+    customer = Customer.objects.get(pk=user_id)
+    form = customer_forms(instance=customer)
+    if request.method == 'POST':
+        form = customer_forms(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('/customers/')
+        context = {
+            'form': form
+        }
+        return render(request, "customers/account_info.html", context)
+
+
+def change_pickup_day(request, customer_id):
+    customer = Customer.objects.get(pk=customer_id)
+    form = change_pickup_form(request.POST, instance=customer)
     if form.is_valid():
-        form.save()
+        form.save(customer)
         return redirect('/customers/')
     context = {
         'form': form
     }
-    return render(request, "customers/customer_signup_information.html", context)
+    return render(request, "customers/customer_change_pickup_day.html", context)
 
+def suspend_account(request, customer_id):
+    pass
 
-def customer_account_info(request, customer_pk):
-    customer = Customer.objects.get(pk=customer_pk)
-    form = customer_forms(request.POST, instance=customer)
-    if form.is_valid():
-        form.save()
-        return redirect('/customers/')
+"""
+def customer_account_info(request):
+    user = request.user
+    customer = Customer.objects.filter(user_id=user.id)
     context = {
-        'form': form
+        'customer': customer
     }
     return render(request, "customers/account_info.html", context)
+"""
 
+
+"""
+user = request.user
+form = customer_forms(request.POST)
+if form.is_valid():
+    form.save()
+    return redirect('/customers/')
+context = {
+    'form': form
+}
+return render(request, "customers/customer_signup_information.html", context)
+"""
