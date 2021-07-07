5fd99a7cb8481c7b0fb0391ab150fd10f1163710
CyrusWarner <cyruswarner27@gmail.com>
2021-07-07 16:32:06 -0500

    Debugged employee home view.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -60,10 +60,7 @@ def employee_home_view(request):
     day_of_the_week = calendar.day_name[curr_date.weekday()]
     todays_customers = []
     for customer in all_customers:
-        if (customer.customer_zip_code == employee_info.employee_zip_code) and (
-                customer.weekly_pickup_day.lower() == day_of_the_week.lower() and (
-                customer.weekly_pickup_confirmed is False
-        ) or customer.onetime_pickup_date == curr_date) and (customer.start_suspension_date is None):
+        if (customer.customer_zip_code == employee_info.employee_zip_code) and (customer.weekly_pickup_day.lower() == day_of_the_week.lower() or customer.onetime_pickup_date == curr_date) and (customer.start_suspension_date is None) and (customer.weekly_pickup_confirmed is False):
             todays_customers.append(customer)
     context = {
         "todays_customers": todays_customers
